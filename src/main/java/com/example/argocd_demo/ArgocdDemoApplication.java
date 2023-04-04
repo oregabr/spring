bc2c111c17677a2248396ce709c00cedd503bc2b
Brendan O Regan <boregan@jaguarlandrover.com>
2023-04-04 16:22:15 +0100

    Test

---
+++ src/main/java/com/example/argocd_demo/ArgocdDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.argocd_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArgocdDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArgocdDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/argocd_demo/ArgocdDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.argocd_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArgocdDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
